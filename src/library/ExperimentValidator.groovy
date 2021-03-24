acac8e3d60069e291c10e5f5bdc40f318321d29d
hbarrerafandino <hbarrerafandino@pinterest.com>
2021-03-23 19:29:37 -0500

    Library v1

--- src/library/ExperimentValidator.groovy
+++ src/library/ExperimentValidator.groovy
@@ -11,4 +11,31 @@ class ExperimentValidator {
         return a + b
     }
 
+    String getDiff() {
+
+        /*
+        def sout = new StringBuilder(), serr = new StringBuilder()
+        //arc export --revision D335558 --unified
+        //arc export --revision D688426 --unified
+        //arc export --revision D691559 --unified
+
+        def proc = """
+                arc export --revision D695602 --unified
+               """.execute()
+
+        proc.consumeProcessOutput(sout, serr)
+        proc.waitForOrKill(5000)
+        return sout
+        println "out> $sout\nerr> $serr"
+        */
+
+        diff = sh (
+                script: """
+                        arc export --revision D335558 --unified
+                        """,
+                returnStdout: true
+        ).trim()
+        return diff
+    }
+
 }
